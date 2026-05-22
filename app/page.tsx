34dada18eb28dfd9af2fa4b96de78665e42cb56a
Sanjay <sanjaybalnad@gmail.com>
2026-05-22 20:04:13 +0530

    chore: rebrand starter to SpendLens and add mongoose

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
-          className="dark:invert"
-          src="/next.svg"
+          // className="dark:invert"
+          src="/logo.png"
           alt="Next.js logo"
           width={100}
           height={20}
